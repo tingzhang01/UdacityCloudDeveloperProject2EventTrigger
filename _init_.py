09f2bcac40b80d4e8161c4e1cdd48978830a5052
tingzhang01 <99369155+tingzhang01@users.noreply.github.com>
2023-01-23 10:04:09 -0500

    Create _init_.py

---
+++ _init_.py
@@ -0,0 +1,14 @@
+import json
+import logging
+
+import azure.functions as func
+
+
+def main(event: func.EventGridEvent):
+    result = json.dumps({
+        'id': event.id,
+        'data': event.get_json(),
+        'topic': event.topic,
+        'subject': event.subject,
+        'event_type': event.event_type,
+    })
